2966cf0c8e6dc3035e37406d414beb166727fc6d
jigarthakkar123 <jigar.thakkar.tops@gmail.com>
2023-07-04 11:10:29 +0530

    4-7-23

---
+++ Django_Project/myapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('contact/',views.contact,name='contact'),
+    path('signup/',views.signup,name='signup'),
+    path('login/',views.login,name='login'),
+    path('logout/',views.logout,name='logout'),
+    path('change-password/',views.change_password,name='change-password'),
+]
